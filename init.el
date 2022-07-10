0c003ac8f21f69cb575764d20ac11b728c35711f
cosmic <kingsleyfrancis42@gmail.com>
2022-07-10 05:40:52 -0400

    custom themes have been addee and more keybinding

--- init.el
+++ init.el
@@ -1,11 +1,10 @@
 (package-initialize)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(global-set-key (kbd "C-q") 'term)
-(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode zenburn-theme json-mode))
+(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode afternoon-theme json-mode))
 (when (cl-find-if-not #'package-installed-p package-selected-packages)
   (package-refresh-contents)
   (mapc #'package-install package-selected-packages))
-(load-theme 'zenburn t)
+(load-theme 'afternoon t)
 (helm-mode)
 (require 'helm-xref)
 (define-key global-map [remap find-file] #'helm-find-files)
@@ -22,23 +21,16 @@
   (require 'dap-chrome)
   (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
   (yas-global-mode))
-(setq make-backup-files nil) ; stop creating backup~ files
-(setq auto-save-default nil) ; stop creating #autosave# files
+;; sample `helm' configuration use https://github.com/emacs-helm/helm/ for details
+(helm-mode)
+(require 'helm-xref)
+(define-key global-map [remap find-file] #'helm-find-files)
+(define-key global-map [remap execute-extended-command] #'helm-M-x)
+(define-key global-map [remap switch-to-buffer] #'helm-mini)
+
 (which-key-mode)
 (add-hook 'c-mode-hook 'lsp)
 (add-hook 'c++-mode-hook 'lsp)
-(menu-bar-mode -1)
-;;stop startup message
-(tool-bar-mode -1) 
-(add-hook 'window-setup-hook 'toggle-frame-maximized t)
-(setq inhibit-splash-screen t)
-(scroll-bar-mode -1)
-(setq c-default-style "bsd"
-      c-basic-offset 8)
-
-(electric-pair-mode 1)
-(electric-indent-mode 1)
-(electric-quote-mode 1)
 
 (setq gc-cons-threshold (* 100 1024 1024)
       read-process-output-max (* 1024 1024)
@@ -49,17 +41,44 @@
 
 (with-eval-after-load 'lsp-mode
   (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
-  (require 'dap-cpptools))
+  (require 'dap-cpptools)
+  (yas-global-mode))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("57e3f215bef8784157991c4957965aa31bac935aca011b29d7d8e113a652b693" default)))
  '(package-selected-packages
-   '(pdf-tools lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode zenburn-theme json-mode)))
+   (quote
+    (afternoon-theme lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode zenburn-theme json-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+;;this are personal configurations i wrote myself
+(menu-bar-mode -1)
+;;stop startup message
+(tool-bar-mode -1) 
+(add-hook 'window-setup-hook 'toggle-frame-maximized t)
+(setq inhibit-splash-screen t)
+(scroll-bar-mode -1)
+(setq c-default-style "bsd"
+      c-basic-offset 8
+      tabs-width 8)
+;;disable creation of backup files
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
+
+;; define custom keybindings here use the example provided below
+(global-set-key (kbd "C-q") 'shell)
+(global-set-key (kbd "C-x q") 'delete-trailing-whitespace)
+
+
+;;font size
+(set-face-attribute 'default nil :height 110)
+(global-display-line-numbers-mode 1)
